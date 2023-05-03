bcb7678bd30227bf350e7765df59733e9fb3c74a
Sagar chand <chandsagar314@gmail.com>
2023-05-03 22:24:25 +0545

    final changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='relative text-3xl text-white mt-20'>About</div>
+    <div className='relative text-white mt-12'>
+      <div className="w-full h-[380px] md:h-[400px]">
+        <div className="w-full h-full ">
+          <div className="absolute w-full h-[380px] md:h-[400px] bg-gradient-to-r from-black"></div>
+          <img className="w-full h-full object-cover" src="images/bgAboutus.jpg" alt="samagra" />
+          <div className="absolute w-full md:top-[15%] top-[5%] flex flex-col items-center gap-y-4 p-2 md:p-5 justify-center">
+            <p className='text-yellow-400 lg:text-2xl'>About Us / Samagra Builders</p>
+            <p className='text-xs md:text-sm text-justify'>Samagra Builders Pvt. Ltd. is an engineering startup company based in Nepal that prepares professional DPRs, engineering designs, and constructs all kinds of buildings and structures. We specialize in identifying and solving common and complex problems related to post construction such as color fading, water penetration, water retention, structural and non-structural cracks, etc. by using modern technology and professional engineers/home inspectors.</p>
+        </div>
+          </div>
+      </div>
+
+    </div>
   )
 }
 
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField } from "@mui/material";
 import { TextareaAutosize } from "@mui/base";
 import { MdEmail, MdPhone } from "react-icons/md";
 import { FaMapMarkerAlt } from "react-icons/fa";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Contact = () => {
+  const [form, setForm] = useState({
+    firstName: "",
+    lastName: "",
+    phone: "",
+    business: "",
+    email: "",
+    message: "",
+  })
+  const toastMsg = ()=>{
+    toast.success('Thank you for submating your details.', {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+      });
+  }
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setForm({ firstName: "", lastName: "", phone: "", business: "", email: "", message: "" })
+    toastMsg();
+  }
   return (
     <div className="relative mt-20 p-2 md:p-3">
+      <ToastContainer />
       <div className="mb-10">
         <p className="text-red-500 font-bold text-lg">Get In Touch</p>
         <p className="text-3xl font-bold">With Us</p>
@@ -46,43 +74,48 @@ const Contact = () => {
         <div className="sm:w-[700px] sm:mx-auto lg:basis-3/5 w-full rounded-md bg-slate-100 shadow-2xl shadow-slate-800 p-5">
           <h1 className="text-base lg:text-xl font-bold text-red-600">Any Questions ?</h1>
           <p className="mb-5 text-base lg:text-xl font-bold">Write Down and Send Us</p>
-          <form>
+          <form onSubmit={submitHandler}>
             <div className="flex flex-col gap-y-6 lg:flex-row lg:justify-between lg:gap-x-2 mb-6 lg:mb-10">
               <TextField
                 required
-                id="outlined-basic"
                 label="First Name"
                 variant="outlined"
+                onChange={(e) => setForm({ ...form, firstName: e.target.value })}
+                value={form.firstName}
               />
               <TextField
                 required
-                id="outlined-basic"
                 label="Last Name"
                 variant="outlined"
+                onChange={(e) => setForm({ ...form, lastName: e.target.value })}
+                value={form.lastName}
               />
               <TextField
                 required
-                id="outlined-basic"
                 label="Phone"
                 variant="outlined"
+                onChange={(e) => setForm({ ...form, phone: e.target.value })}
+                value={form.phone}
               />
             </div>
             <div className="mb-6 lg:mb-10">
               <TextField
                 required
-                id="outlined-basic"
                 label="Business / Organization"
                 variant="outlined"
                 fullWidth
+                onChange={(e) => setForm({ ...form, business: e.target.value })}
+                value={form.business}
               />
             </div>
             <div className="mb-6 lg:mb-10">
               <TextField
                 required
-                id="outlined-basic"
                 label="email"
                 variant="outlined"
                 fullWidth
+                onChange={(e) => setForm({ ...form, email: e.target.value })}
+                value={form.email}
               />
             </div>
             <TextareaAutosize
@@ -90,6 +123,8 @@ const Contact = () => {
               placeholder="Write Your Message here"
               className="w-full bg-slate-300 rounded-md focus:outline-none p-5"
               minRows={5}
+              onChange={(e) => setForm({ ...form, message: e.target.value })}
+              value={form.message}
             />
             <button
               type="submit"
@@ -98,8 +133,8 @@ const Contact = () => {
               Submit
             </button>
             <button
-              type="reset"
               className="mt-4 ml-7 bg-sky-500 py-3 px-7 rounded-xl"
+              onClick={()=>setForm({ firstName: "", lastName: "", phone: "", business: "", email: "", message: "" })}
             >
               Reset
             </button>
